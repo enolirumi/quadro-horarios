3a617e069ecd3e37c98ad187df5e3417cc853224
Enzo Russo <enzorussomiranda@gmail.com>
2022-11-07 23:26:26 -0300

    Antes de dormir

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -42,37 +42,42 @@ const Home = () => {
     }
 
     useEffect(() => {
-        
+
     }, [])
 
     const [nomeProfessor, setNomeProfessor] = useState('')
     const [nomeTurma, setNomeTurma] = useState('')
     const [nomeDisciplina, setNomeDisciplina] = useState('')
 
+    const [modal, setModal] = useState(false)
+
     return (
-        <section className={styles['section-cadastro-infos']}>
-            <div className={`${styles['box']}`}>
-                <h3>Cadastro de professor</h3>
-                <div className={styles['line']}>
-                    <input type="text" placeholder='Nome do professor:' onChange={(e) => setNomeProfessor(e.target.value)} value={nomeProfessor} />
+        <>
+            {modal && <div className={styles['modal']}></div>}
+            <section className={styles['section-cadastro-infos']}>
+                <div className={`${styles['box']}`}>
+                    <h3>Cadastro de professor</h3>
+                    <div className={styles['line']}>
+                        <input type="text" placeholder='Nome do professor:' onChange={(e) => setNomeProfessor(e.target.value)} value={nomeProfessor} />
+                    </div>
+                    <div className={styles['btn-cadastrar']} onClick={() => cadastraProfessor()}>Cadastrar</div>
                 </div>
-                <div className={styles['btn-cadastrar']} onClick={() => cadastraProfessor()}>Cadastrar</div>
-            </div>
-            <div className={`${styles['box']}`}>
-                <h3>Cadastro de Disciplina</h3>
-                <div className={styles['line']}>
-                    <input type="text" placeholder='Nome da disciplina:' onChange={(e) => setNomeDisciplina(e.target.value)} value={nomeDisciplina} />
+                <div className={`${styles['box']}`}>
+                    <h3>Cadastro de Disciplina</h3>
+                    <div className={styles['line']}>
+                        <input type="text" placeholder='Nome da disciplina:' onChange={(e) => setNomeDisciplina(e.target.value)} value={nomeDisciplina} />
+                    </div>
+                    <div className={styles['btn-cadastrar']} onClick={() => cadastraDisciplina()}>Cadastrar</div>
                 </div>
-                <div className={styles['btn-cadastrar']} onClick={() => cadastraDisciplina()}>Cadastrar</div>
-            </div>
-            <div className={`${styles['box']}`}>
-                <h3>Cadastro de Turma</h3>
-                <div className={styles['line']}>
-                    <input type="text" placeholder='Nome da turma:' onChange={(e) => setNomeTurma(e.target.value)} value={nomeTurma} />
+                <div className={`${styles['box']}`}>
+                    <h3>Cadastro de Turma</h3>
+                    <div className={styles['line']}>
+                        <input type="text" placeholder='Nome da turma:' onChange={(e) => setNomeTurma(e.target.value)} value={nomeTurma} />
+                    </div>
+                    <div className={styles['btn-cadastrar']} onClick={() => cadastraTurma()}>Cadastrar</div>
                 </div>
-                <div className={styles['btn-cadastrar']} onClick={() => cadastraTurma()}>Cadastrar</div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
